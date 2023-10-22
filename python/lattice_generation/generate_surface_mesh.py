57bdc8a8ce08c9370ad980a6f3d07064fc584a2a
Chris <zhiyucheng2@gmail.com>
2023-10-22 11:27:30 +0000

    finish creating tetrahedralize optiosn

--- python/lattice_generation/generate_surface_mesh.py
+++ python/lattice_generation/generate_surface_mesh.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import pymesh as pm
 import numpy as np
 import sys
@@ -65,7 +66,7 @@ def get_wireframe(mesh, max_volume_ratio):
 
 
 def get_wire_info_prototype(mesh):
-    return np.dot(mesh.vertices, 3), mesh.faces
+    return np.dot(mesh.vertices, 1), mesh.faces
 
 def edges_from_faces(vertices, faces):
     edges = []
@@ -101,6 +102,7 @@ def get_wire_info(vertices, faces, max_volume_ratio):
     tet.points = vertices
     tet.triangles = faces
     # tet.keep_convex_hull = True
+
     def slider_to_volume(vertices, n):
         min_corner = np.min(vertices, axis=0)
         max_corner = np.max(vertices, axis=0)
@@ -156,9 +158,17 @@ def meshify_inside(inner_points):
 #     Default = 1
 #     Delaunay = 1
 
-# class triangulation_option:
-#     pass
-#     #TODO may not implement
+class tetrahedralize_options(Enum):
+    """defines an enum class that represents the different engines that are 
+    available in the pymesh tetrahedralize function""" 
+    TETGEN_ADVANCED = 'default'
+    tetgen = 'tet'
+    quartet = 'qt'
+    delpsc = 'dpc'
+    mmg = 'mmg'
+    tetwild = 'ttd'
+
+
 
     
 
@@ -168,44 +178,59 @@ def parse_args():
     wire_thickness = sys.argv[2] if len(sys.argv) > 2 else None
     #corresponds to tesellation size
     max_volume_ratio = sys.argv[3] if len(sys.argv) > 3 else None
-    tessellation_option = sys.argv[4]
-    node_placement_algo_option = sys.argv[5] 
-    # triangulation_option = sys.argv[6]
     
+    #if you only have 3 arguments then the program will use advanced tetgen
+    tessellation_option = tetrahedralize_options(sys.argv[4]) if len(sys.argv) > 4 else tetrahedralize_options.TETGEN_ADVANCED
 
-    if file_path is None or wire_thickness is None or max_volume_ratio is None or tessellation_option is None or node_placement_algo_option is None:
+
+    #do not need the 5 & 6th args for tetgen advanced
+
+    if tessellation_option != tetrahedralize_options.TETGEN_ADVANCED:
+        cell_size = sys.argv[5] if len(sys.argv) > 5 else None
+        radius_edge_ratio = sys.argv[6] if len(sys.argv) > 6 else 2.0
+    else:
+        cell_size = 0 # just so it can pass the if check
+        radius_edge_ratio = 0 # just so it can pass the if check
+
+    
+    if file_path is None or wire_thickness is None or max_volume_ratio is None or tessellation_option is None or cell_size is None:
         print("Usage: python generate_surface_mesh.py <filepath> <wire thickness> <longest edge> <tessellation_option> <node_placement_algo_option> <triangulation_option>")
         exit()
 
     else:
         #TODO actually implenent the usage of options
-        return file_path, float(wire_thickness), float(max_volume_ratio), tessellation_option, node_placement_algo_option
+        return file_path, float(wire_thickness), float(max_volume_ratio), tessellation_option, float(cell_size), float(radius_edge_ratio)
 
 
 if __name__ == "__main__":
-    path, thickness, max_volume_ratio, tesellation_option, node_placement_algo_option = parse_args()
+    path, thickness, max_volume_ratio, tesellation_option, cell_size, radius_edge_ratio = parse_args()
+    
     mesh = pm.load_mesh(path)
-
     print("dim, vertex_per_face, vertex_per_voxel")
     print(mesh.dim, mesh.vertex_per_face, mesh.vertex_per_voxel)
 
-    print("creating wireframe")
-    vertices, edges = get_wireframe(mesh, max_volume_ratio)
+    if tesellation_option == tetrahedralize_options.TETGEN_ADVANCED:
 
-    #this command takes hella long and sometimes gets killed by docker
-    wire_network = pm.wires.WireNetwork.create_from_data(vertices, edges)
-    print_wire_data(wire_network)
+        print("creating wireframe")
+        vertices, edges = get_wireframe(mesh, max_volume_ratio)
+
+        #this command takes hella long and sometimes gets killed by docker
+        wire_network = pm.wires.WireNetwork.create_from_data(vertices, edges)
+        print_wire_data(wire_network)
+        
+        # Inflator
+        inflator = pm.wires.Inflator(wire_network)
+        
+        print("starting inflation")
+
+        inflator.inflate(thickness, allow_self_intersection=True)
+        mesh = inflator.mesh
+
+    else:
+        print("starting inflation")
+        print(tesellation_option.name)
+        mesh = pm.tetrahedralize(mesh, cell_size, radius_edge_ratio, engine=tesellation_option.name)
     
-    # Inflator
-    inflator = pm.wires.Inflator(wire_network)
-    
-    print("starting inflation")
-
-    inflator.inflate(thickness, allow_self_intersection=True)
-    mesh = inflator.mesh
-
     print("inflated, saving now")
     # save the mesh
     pm.save_mesh("Output.stl", mesh)
-
-    
