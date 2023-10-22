48ad59c816d96c1d7db25fb27d5fbb049c6e0f16
hung-ngm <prohungnguyenminh@gmail.com>
2023-10-22 14:26:10 +1100

    fix: streamlit work with older version

--- python/frontend/gui.py
+++ python/frontend/gui.py
@@ -17,37 +17,33 @@ def launch_main_win():
     </style>
     """, unsafe_allow_html=True)
 
-    # Left column
+    # Create main columns
     left_col, right_col = st.columns([2, 1])  # Adjust the ratio of the columns
 
     with left_col:
         st.subheader("Tessellation")  # Change to subheader for smaller text
-        col_1, col_2 = st.columns([1, 1])  # Create two columns side by side
-        with col_1:
-            if st.button("Kagome"):
-                st.write("Kagome selected")
-            if st.button("Tetrahedral"):
-                st.write("Tetrahedral selected")
-            if st.button("Icosahedral"):
-                st.write("Icosahedral selected")
-            if st.button("Voronoi"):
-                st.write("Voronoi selected")
-            if st.button("Rhombic"):
-                st.write("Rhombic selected")
+        if st.button("Kagome"):
+            st.write("Kagome selected")
+        if st.button("Tetrahedral"):
+            st.write("Tetrahedral selected")
+        if st.button("Icosahedral"):
+            st.write("Icosahedral selected")
+        if st.button("Voronoi"):
+            st.write("Voronoi selected")
+        if st.button("Rhombic"):
+            st.write("Rhombic selected")
 
         st.subheader("Node placement algorithm")  # Change to subheader for smaller text
-        col1, col2 = st.columns([1, 1])  # Create two columns side by side
-        with col1:
-            if st.button("Delaunay Triangulation"):
-                st.write("Delaunay Triangulation selected")
-            if st.button("Voronoi Triangulation"):
-                st.write("Voronoi Triangulation selected")
+        if st.button("Delaunay Triangulation"):
+            st.write("Delaunay Triangulation selected")
+        if st.button("Voronoi Triangulation"):
+            st.write("Voronoi Triangulation selected")
 
-        with col2:
-            if st.button("Minimum Weight Triangulation"):
-                st.write("Minimum Weight Triangulation selected")
-            if st.button("Bowyer-Watson Triangulation"):
-                st.write("Bowyer-Watson Triangulation selected")
+        st.subheader("Triangulation")  # Change to subheader for smaller text
+        if st.button("Minimum Weight Triangulation"):
+            st.write("Minimum Weight Triangulation selected")
+        if st.button("Bowyer-Watson Triangulation"):
+            st.write("Bowyer-Watson Triangulation selected")
 
         # Slider for edge size
         edge_size = st.slider("Edge size", min_value=0.0, max_value=10.0, value=1.0)
