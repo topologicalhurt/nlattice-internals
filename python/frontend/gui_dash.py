a938deb68942e1ebb70dbceb7158bdd6127800a0
Hieu Bui <tbui0355@uni.sydney.edu.au>
2023-10-23 00:30:54 +1100

    Implemented slider label

--- python/frontend/gui_dash.py
+++ python/frontend/gui_dash.py
@@ -5,7 +5,7 @@ from meshlib import mrmeshpy as mm
 
 from python.pc.consts import Conf
 from python.frontend.view_model import visualise
-from python.lattice_generation.generate_surface_mesh import transform_mesh
+from python.lattice_generation.generate_surface_mesh import transform_mesh, tetrahedralize_options
 
 # def launch_dash_gui():
 app = Dash(__name__)
@@ -50,20 +50,24 @@ app.layout = html.Div([
         html.H2("Edge size"),
         dcc.Slider(
             id='edge-size',
-            min=0,
-            max=10,
+            min=0.0,
+            max=10.0,
             step=0.1,
-            value=1
+            value=1.0,
+            marks={i: str(i) for i in range(0, 11)},
         ),
+        html.Div(id='edge-size-label'),
 
         html.H2("Tessellation size"),
         dcc.Slider(
             id='tess-size',
-            min=0,
-            max=10,
+            min=0.0,
+            max=10.0,
             step=0.1,
-            value=1
+            value=1.0,
+            marks={i: str(i) for i in range(0, 11)},
         ),
+        html.Div(id='tess-size-label'),
 
         html.Button('Convert', id='convert-button', n_clicks=0)
     ], style={'width': '49%', 'display': 'inline-block'}),
@@ -75,11 +79,11 @@ app.layout = html.Div([
 ])
 
 @callback(
-    Output('mesh-graph', 'figure'),
-    [Input('convert-button', 'n_clicks')],
-    [State('edge-size', 'value'),
-     State('tess-size', 'value'),
-     State('tess-option', 'value'),
+    [Output('mesh-graph', 'figure'),
+    Output('edge-size-label', 'children'),
+    Output('tess-size-label', 'children')],
+    [Input('convert-button', 'n_clicks'), Input('edge-size', 'value'), Input('tess-size', 'value')],
+    [State('tess-option', 'value'),
      State('node-placement-algo-option', 'value')]
 )
 
@@ -93,21 +97,23 @@ def update_output(n_clicks, edge_size, tess_size, tess_option, node_placement_al
         print(tess_size)
         print(tess_option)
         print(node_placement_algo_option)
+        
 
-        # transform_mesh(
-        #     path=file_path,
-        #     thickness=edge_size,
-        #     max_volume_ratio=tess_size,
-        #     tesellation_option=tess_option,
-        #     node_placement_algo_option=node_placement_algo_option
-        # )
+        transform_mesh(
+            path=file_path,
+            thickness=edge_size,
+            max_volume_ratio=tess_size,
+            tesellation_option=tetrahedralize_options.TETGEN_ADVANCED,
+            cell_size=0,
+            radius_edge_ratio=0
+        )
 
-        file_path = "./python/lattice_generation/output.stl"
+        file_path = "./output.stl"
         
         
     mesh = mm.loadMesh(mm.Path(file_path))
     fig = visualise(mesh, edge_size, tess_size)
-    return fig
+    return fig, html.Div(f"Current edge size: {edge_size}"), html.Div(f"Current tess size: {tess_size}")
 
      
 
