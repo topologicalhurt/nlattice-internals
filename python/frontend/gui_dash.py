6bd16f4295caa0bc098cb2fb42a0b9b695c1d9c3
Julian Tyl <julian.tyl@hotmail.com>
2023-10-31 15:04:27 +1100

    final

--- python/frontend/gui_dash.py
+++ python/frontend/gui_dash.py
@@ -6,8 +6,10 @@ from meshlib import mrmeshpy as mm
 from python.pc.consts import Conf
 from python.frontend.view_model import visualise
 from python.lattice_generation.generate_surface_mesh import transform_mesh, tetrahedralize_options
+import base64
 
 app = Dash(__name__)
+global_path = None
 
 
 def launch_dash_gui():
@@ -30,6 +32,28 @@ app.layout = html.Div([
             ],
             value='Kagome'
         ),
+
+        html.H2("Select File"),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            multiple=False
+        ),
+        html.Div(id='output-container-button'),
+
 
         html.H2("Node placement algorithm"),
         dcc.RadioItems(
@@ -58,7 +82,7 @@ app.layout = html.Div([
             max=1.0,
             step=0.05,
             value=0.1,
-            marks={i/10: str(i/10) for i in range(0, 11)},
+            marks={i / 10: str(i / 10) for i in range(0, 11)},
         ),
         html.Div(id='edge-size-label'),
 
@@ -69,7 +93,7 @@ app.layout = html.Div([
             max=100.0,
             step=1.0,
             value=1.0,
-            marks={i*10: str(i*10) for i in range(0, 11)},
+            marks={i * 10: str(i * 10) for i in range(0, 11)},
         ),
         html.Div(id='tess-size-label'),
 
@@ -85,25 +109,46 @@ app.layout = html.Div([
 
 @callback(
     [Output('mesh-graph', 'figure'),
-    Output('edge-size-label', 'children'),
-    Output('tess-size-label', 'children')],
+     Output('output-container-button', 'children')],
+    Input('upload-data', 'filename'))
+def update_graph(filename):
+    global global_path
+    if filename is not None:
+        global_path = "python/lattice_generation/stl_assets/" + filename
+        try:
+            mesh = mm.loadMesh(mm.Path(global_path))
+            fig = visualise(mesh, 0.1, 1)
+            return fig, 'File found'
+        except Exception as e:
+            print(e)
+            global_path = "python/lattice_generation/stl_assets/20mm_cube.stl"
+            mesh = mm.loadMesh(mm.Path(global_path))
+            fig = visualise(mesh, 0.1, 1)
+            return fig, 'default File loaded'
+    else:
+        # default filepath
+        global_path = "python/lattice_generation/stl_assets/20mm_cube.stl"
+        mesh = mm.loadMesh(mm.Path(global_path))
+        fig = visualise(mesh, 0.1, 1)
+        return fig, 'default File load'
+
+
+@callback(
+    [Output('edge-size-label', 'children'),
+     Output('tess-size-label', 'children')],
     [Input('convert-button', 'n_clicks'), Input('edge-size', 'value'), Input('tess-size', 'value')],
     [State('tess-option', 'value'),
      State('node-placement-algo-option', 'value')]
 )
 def update_output(n_clicks, edge_size, tess_size, tess_option, node_placement_algo_option):
-    original_path = "./python/lattice_generation/stl_assets/bulbasaur.stl"
-    # original_path = "./python/lattice_generation/stl_assets/20mm_cube.stl"
-    
-    if "convert-button" == ctx.triggered_id:
+    if "convert-button" == ctx.triggered_id and global_path is not None:
         print(edge_size)
         print(tess_size)
         print(tess_option)
         print(node_placement_algo_option)
-        
 
         transform_mesh(
-            path=original_path,
+            path=global_path,
             thickness=edge_size,
             max_volume_ratio=tess_size,
             tesellation_option=tetrahedralize_options.TETGEN_ADVANCED,
@@ -111,8 +156,4 @@ def update_output(n_clicks, edge_size, tess_size, tess_option, node_placement_al
             radius_edge_ratio=0
         )
 
-        file_path = "./output.stl"
-
-    mesh = mm.loadMesh(mm.Path(original_path))
-    fig = visualise(mesh, edge_size, tess_size)
-    return fig, html.Div(f"Current edge size: {edge_size}"), html.Div(f"Current test size: {tess_size}")
+    return html.Div(f"Current edge size: {edge_size}"), html.Div(f"Current test size: {tess_size}")
